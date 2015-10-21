9f936e79bdc56fa897f197d5a25caaac409535d2
peng <dianxiangan@aliyun.com>
2015-10-21 17:18:56 +0800

    simplify project structure

--- src/main/java/psyco/codebot/api/rest/WebMain.java
+++ src/main/java/psyco/codebot/api/rest/WebMain.java
@@ -3,7 +3,6 @@ package psyco.codebot.api.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -11,7 +10,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@SpringBootApplication
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @ComponentScan(basePackages = {"psyco.codebot.api.rest"})
 public class WebMain {
